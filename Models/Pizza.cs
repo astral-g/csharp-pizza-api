a893e39b74091ab998ea7505d45e8db1ddebb3d1
astral-g <gfield991@gmail.com>
2022-03-02 19:39:45 +0000

    Added logic for the order functionality to the pizza service

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -6,3 +6,11 @@ public class Pizza
     public string? Name { get; set; }
     public bool IsGlutenFree { get; set; }
 }
+
+public class CustomPizza : Pizza
+{
+    public int CustomId { get; set; }
+    public string? Toppings { get; set; }
+    public string? Base { get; set; }
+    public string? Sauce { get; set; }
+}
--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -1,17 +1,27 @@
 using GfPizza.Models;
+using System.Runtime.InteropServices;
 
 namespace GfPizza.Services;
 
 public static class PizzaService
 {
     static List<Pizza> Pizzas { get; }
+    static List <CustomPizza> CustomPizzas { get; set; }
+    static List <Pizza> OrderQueue { get; set; }
     static int nextId = 3;
+    static int nextCustomId = 1;
     static PizzaService()
     {
         Pizzas = new List<Pizza>
         {
             new Pizza { Id = 1, Name = "Classic Italian", IsGlutenFree = false },
             new Pizza { Id = 2, Name = "Vegan", IsGlutenFree = true }
+        };
+
+        CustomPizzas = new List<CustomPizza> {};
+
+        OrderQueue = new List<Pizza> {
+            new CustomPizza {}
         };
     }
 
@@ -42,4 +52,13 @@ public static class PizzaService
 
             Pizzas[index] = pizza;
         }
+
+    public static void Order(bool isCustom, [Optional]Pizza pizza, [Optional]CustomPizza customPizza)
+    {
+        if(isCustom is true) {
+            OrderQueue.Add(customPizza);
+        }
+
+        OrderQueue.Add(pizza);
+    }
 }
